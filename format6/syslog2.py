89aecdf1752907fe2bc29114cca10d9ee12366bb
sujonict07 <sujonict07@gmail.com>
2015-08-01 12:31:08 +0600

    update syslog in 1.8.15

--- format6/syslog2.py
+++ format6/syslog2.py
@@ -1,13 +1,16 @@
 import logging
 import syslog
+import socket
 from logging.handlers import SysLogHandler
 
-logger = logging.getLogger('syslogTest')
+logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-syslog = SysLogHandler(address='/dev/log', facility=syslog.LOG_LOCAL6)
+# sysl = SysLogHandler(address='/dev/log',facility=syslog.LOG_USER)
+sysl = SysLogHandler(address=('127.0.0.1', 514), facility=syslog.LOG_USER, socktype=socket.SOCK_DGRAM)
 formatter = logging.Formatter('%(name)s: %(levelname)s %(message)s')
-syslog.setFormatter(formatter)
-logger.addHandler(syslog)
+sysl.setFormatter(formatter)
+logger.addHandler(sysl)
+logger.addHandler(logging.FileHandler("hits.log"))
 
 logger.debug('this is test of syslog using debug')
 logger.info('this is test of syslog using info')
